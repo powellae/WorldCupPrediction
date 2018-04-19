d2595b4b9030c990c4a39d1e083fdb59a5fc49d7
ambrosend <35616737+ambrosend@users.noreply.github.com>
2018-04-19 13:04:17 -0400

    Add files via upload

--- WorldCup18.R
+++ WorldCup18.R
@@ -9,7 +9,7 @@
 library(dplyr)
 
 #Data
-soccer.db <- read.csv("Documents/Analytics/soccer_db.csv")
+soccer.db <- read.csv("soccer_db.csv")
 
 #Cleaning Data
 soccer.db$Home <- gsub("\\ - .*", "", soccer.db$Match)
@@ -26,10 +26,28 @@ soccer.db$Loc.Home <- ifelse(soccer.db$Location == soccer.db$Home, 1, 0)
 soccer.db <- soccer.db %>%
   filter(Tournament != "Amateur")
 
+#Remove UNIFFAC Cup
+soccer.db <-soccer.db %>%
+  filter(Tournament != "UNIFFAC Cup")
+
 #Analyzing One Team
 Germany <- soccer.db %>%
   filter(Home == "Germany" | Away == "Germany")
+60/210
+b=50/210
+#Adding in rankings for each cup
+soccer.db_ext<-soccer.db %>% mutate(Rate=case_when (Tournament=="FIFA World Cup"~ (60/210), Tournament=="Confederations Cup"~ (50/210),Tournament=="UEFA Euro"|
+                                                    Tournament=="AFC Asian Cup"|Tournament=="Copa América"|Tournament=="CONCACAF Championship"|Tournament=="Oceania Nations Cup"~(40/210),
+                                                    Tournament=="African Cup of Nations"|Tournament=="Gold Cup"|Tournament=="UNCAF Cup"|Tournament=="UAFA Cup"|
+                                                    Tournament=="CCCF Championship"|Tournament=="NAFU Championship"~(30/210),
+                                                    Tournament=="CFU Caribbean Cup"|Tournament=="CECAFA Cup"|Tournament=="AFF Championships"|Tournament=="COSAFA Cup"|
+                                                    Tournament=="South Pacific Games"|Tournament=="AFC Challenge Cup"|Tournament=="SAFF Cup"|Tournament=="EAFF Cup"|
+                                                    Tournament=="WAFF Championship"~(20/210),Tournament=="Friendly"~(10/210)))
 
+
+
+
+Tournament=="CFU Caribbean Cup"|Tournament=="CECAFA Cup"|
 #Looking at Home Team Effect
 HomeTeams <- soccer.db %>%
   filter(Loc.Home == 1) %>%
@@ -39,3 +57,4 @@ HomeTeams <- soccer.db %>%
   dplyr::select(Location, avg.score, number) %>%
   unique() %>%
   filter(number >= 50)
+
